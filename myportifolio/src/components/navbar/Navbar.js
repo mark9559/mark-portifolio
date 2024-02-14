1d9e2f9d14df1b655218e319776d59051786ccf7
mark9559 <mwangimark57@gmail.com>
2024-02-14 23:54:03 +0300

    Adding Floating Nav

--- myportifolio/src/components/navbar/Navbar.js
+++ myportifolio/src/components/navbar/Navbar.js
@@ -1,45 +1,79 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import ThemePicker from '../themepicker/ThemePicker';
-import './navbar.css'
-import { BsFillPaletteFill } from 'react-icons/bs'; // Import an icon from React Icons
+import { FaBars, FaTimes } from 'react-icons/fa';
 
+const FlowbiteNavbar = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
 
-const Navbar = () => {
-  const [showThemePicker, setShowThemePicker] = useState(false);
-
-  const toggleThemePicker = () => {
-    setShowThemePicker(!showThemePicker);
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
   };
 
   return (
-    <nav>
-      <div className="logo">
-        <Link to="/">MARK T</Link>
+    <nav className="bg-gradient-to-r from-purple-600 via-indigo-600 to-blue-600 p-4 rounded-full  w-full z-50">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <Link to="/" className="text-white text-xl lg:text-2xl font-bold">
+          M.M
+        </Link>
+
+        {/* Hamburger Icon for Small Screens */}
+        <div className="lg:hidden">
+          <button className="text-white focus:outline-none" onClick={toggleSidebar}>
+            <FaBars className="theme-btn" />
+          </button>
+        </div>
+
+        {/* Links for Larger Screens */}
+        <div className="hidden lg:flex space-x-4">
+          <Link to="/" className="text-white hover:text-gray-300 transition duration-300">
+            Home
+          </Link>
+          <Link to="/about" className="text-white hover:text-gray-300 transition duration-300">
+            About
+          </Link>
+          <Link to="/services" className="text-white hover:text-gray-300 transition duration-300">
+            Services
+          </Link>
+          <Link to="/projects" className="text-white hover:text-gray-300 transition duration-300">
+            Projects
+          </Link>
+          <Link to="/contact" className="text-white hover:text-gray-300 transition duration-300">
+            Contact
+          </Link>
+        </div>
       </div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/services">Services</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <button className='theme-btn' onClick={toggleThemePicker}><BsFillPaletteFill /></button>
-        </li>
-      </ul>
-      {showThemePicker && <ThemePicker />}
+
+      {/* Sidebar for Small Screens */}
+      {showSidebar && (
+        <div className="lg:hidden fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-90 flex flex-col items-center">
+          <button
+            className="absolute bottom-4 left-4 text-white text-2xl focus:outline-none"
+            onClick={toggleSidebar}
+          >
+            <FaTimes />
+          </button>
+          <div className="text-white text-2xl font-bold mt-12">
+            <Link to="/" className="block py-2 hover:text-gray-300 transition duration-300">
+              Home
+            </Link>
+            <Link to="/about" className="block py-2 hover:text-gray-300 transition duration-300">
+              About
+            </Link>
+            <Link to="/services" className="block py-2 hover:text-gray-300 transition duration-300">
+              Services
+            </Link>
+            <Link to="/projects" className="block py-2 hover:text-gray-300 transition duration-300">
+              Projects
+            </Link>
+            <Link to="/contact" className="block py-2 hover:text-gray-300 transition duration-300">
+              Contact
+            </Link>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
 
-export default Navbar;
+export default FlowbiteNavbar;
