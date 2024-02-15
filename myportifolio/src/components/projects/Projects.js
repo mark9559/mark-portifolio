944fb149f0c62cf5b70947256f4def7977fe0e7f
mark9559 <mwangimark57@gmail.com>
2024-02-16 00:28:21 +0300

    Created Projects Cards

--- myportifolio/src/components/projects/Projects.js
+++ myportifolio/src/components/projects/Projects.js
@@ -1,7 +1,7 @@
-// components/Projects/Projects.js
-
 import React from 'react';
 import './projects.css';
+import { Button } from 'flowbite-react';
+
 
 const Projects = () => {
   const projects = [
@@ -33,24 +33,28 @@ const Projects = () => {
   ];
 
   return (
-    <div className="projects-container">
-      <h2>Projects</h2>
-      <div className="project-cards">
-        {projects.map((project) => (
-          <div key={project.id} className="project-card">
-            <img src={project.thumbnail} alt={project.title} />
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
-            <div className="project-links">
-              <a href={project.demoLink} target="_blank" rel="noopener noreferrer">
-                Demo
-              </a>
-              <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
-                GitHub
-              </a>
+    <div className="projects-container py-16 ">
+      <div className="container mx-auto">
+        <h2 className="text-3xl font-bold mb-10 mt-10 text-purple-600">Projects</h2>
+        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-8">
+          {projects.map((project) => (
+            <div key={project.id} className="projects-card rounded-lg overflow-hidden shadow-lg mb-8">
+              <img className="w-full h-40 object-cover" src={project.thumbnail} alt={project.title} />
+              <div className="p-6">
+                <h3 className="text-xl font-bold mb-2">{project.title}</h3>
+                <p className="text-gray-600 mb-4">{project.description}</p>
+                <div className="flex space-x-4">
+                  <Button color="purple" pill>
+                    Demo
+                  </Button>
+                  <Button color="purple" pill>
+                    Github
+                  </Button>
+                </div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
