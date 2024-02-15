603ed488ebe701706c2f664dcc3136d7f45d0cd7
mark9559 <mwangimark57@gmail.com>
2024-02-15 19:46:49 +0300

    Adding Layout Route

--- myportifolio/src/App.js
+++ myportifolio/src/App.js
@@ -1,38 +1,35 @@
 // App.js
 
 import React from 'react';
-import './App.css'; // Import the combined CSS file
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import Route and Routes from react-router-dom
-import Navbar from './components/navbar/Navbar';
+import './App.css'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Projects from './components/projects/Projects';
 import Contact from './components/contact/Contact';
-import ThemePicker from './components/themepicker/ThemePicker.js';
 import Services from './components/services/Services.js';
-// import './index.css'
-import CustomFooter from './components/Footer.js';
-import FlowbiteNavbar from './components/navbar/Navbar';
-import FlowbiteFooter from './components/Footer.js';
+import Layout from './layout/Layout.js';
+
 
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <FlowbiteNavbar />
-
 
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
+
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Route> 
+
         </Routes>
 
 
-        <FlowbiteFooter />
       </div>
     </BrowserRouter>
   );
